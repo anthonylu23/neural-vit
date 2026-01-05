355dfdb2b3f53ba71d4fc09453772c9c9810e986
Anthony Lu <ayl2063@nyu.edu>
2026-01-05 13:01:16 -0600

    experiments clarification

--- temporal_vit/training/train.py
+++ temporal_vit/training/train.py
@@ -286,6 +286,9 @@ def main():
         use_preprocessed=True,
         stats_path=None,
         output_dir=output_dir,
+        experiment_name="lfp-temporal-vit-experiments",
+        project_id="lfp-temporal-vit",
+        location="us-central1",
     )
     train(config)
 
